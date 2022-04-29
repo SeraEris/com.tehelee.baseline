859486b0d290fe063b3a08477f46127cc2eb2a46
Tehelee <Shaander1@gmail.com>
2022-04-29 08:05:43 -0700

    Expanded key input support

---
+++ Runtime/Globals/KeyBindings.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+namespace Tehelee.Baseline
+{
+    public class KeyBindings
+	{
+		private const string jsonFileName = "KeyBindings.json";
+
+		public static bool hasLoaded { get; private set; }
+		private static bool loading = false;
+		private static int saveRequests = 0;
+
+		public static string GetJsonFilePath() =>
+			System.IO.Path.Combine( Application.persistentDataPath, jsonFileName );
+
+		private static int enabledCount = 0;
+		public static Dictionary<string, KeyCode[]> binds = new Dictionary<string, KeyCode[]>();
+		
+		public static KeyCode[] GetKeyCodes( string key )
+		{
+			if( binds.ContainsKey( key ) )
+				return new List<KeyCode>( binds[ key ] ).ToArray();
+			else
+				return new KeyCode[ 0 ];
+		}
+
+		public static void SetKeyCodes( string key, KeyCode[] keyCodes )
+		{
+			if( string.IsNullOrWhiteSpace( key ) )
+				return;
+
+			HashSet<KeyCode> hashSet = new HashSet<KeyCode>();
+			for( int i = 0, iC = keyCodes.Length; i < iC; i++ )
+				if( keyCodes[ i ] != KeyCode.None )
+					hashSet.Add( keyCodes[ i ] );
+
+			keyCodes = new List<KeyCode>( hashSet ).ToArray();
+
+			if( keyCodes.Length == 0 )
+			{
+				if( binds.ContainsKey( key ) )
+					binds.Remove( key );
+			}
+			else
+			{
+				if( binds.ContainsKey( key ) )
+					binds[ key ] = keyCodes;
+				else
+					binds.Add( key, keyCodes );
+			}
+		}
+
+		public static void OnEnable()
+		{
+			if( enabledCount++ == 0 )
+			{
+				Load();
+			}
+		}
+
+		public static void OnDisable()
+		{
+			Save();
+		}
+
+		public static void Save()
+		{
+			saveRequests++;
+
+			if( saveRequests > 1 )
+				return;
+
+			_Save();
+		}
+
+		private static void _Save()
+		{
+			string jsonFilePath = GetJsonFilePath();
+
+			string jsonFileDirectory = Path.GetDirectoryName( jsonFilePath );
+
+			if( File.Exists( jsonFilePath ) )
+				File.Delete( jsonFilePath );
+
+			if( !Directory.Exists( jsonFileDirectory ) )
+				Directory.CreateDirectory( jsonFileDirectory );
+
+			if( Directory.Exists( jsonFileDirectory ) && !File.Exists( jsonFilePath ) )
+			{
+				using( StreamWriter streamWriter = File.CreateText( jsonFilePath ) )
+				{
+					JsonTextWriter jsonWriter = new JsonTextWriter( streamWriter );
+					jsonWriter.Formatting = Formatting.Indented;
+
+					JObject jObj = new JObject();
+
+					List<string> bindKeys = new List<string>( binds.Keys );
+					bindKeys.Sort();
+
+					foreach( string bindKey in bindKeys )
+					{
+						KeyCode[] keyCodes = binds[ bindKey ];
+
+						if( keyCodes.Length > 0 )
+						{
+							JArray jArray = new JArray();
+
+							foreach( KeyCode keyCode in keyCodes )
+								jArray.Add( keyCode.ToString() );
+
+							jObj.Add( bindKey, jArray );
+						}
+					}
+
+					jObj.WriteTo( jsonWriter );
+				}
+			}
+
+			saveRequests = 0;
+		}
+
+		[RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSceneLoad )]
+		public static void Load()
+		{
+			if( loading )
+				return;
+
+			loading = true;
+
+			string jsonFilePath = GetJsonFilePath();
+
+			Utils.WaitForTask( new Task<Dictionary<string,KeyCode[]>>( () =>
+			{
+				Dictionary<string, KeyCode[]> binds = new Dictionary<string, KeyCode[]>();
+
+				if( File.Exists( jsonFilePath ) )
+				{
+					using( StreamReader streamReader = new StreamReader( jsonFilePath ) )
+					{
+						JsonTextReader jsonReader = new JsonTextReader( streamReader );
+
+						JObject jObj = null;
+
+						try
+						{
+							jObj = ( JObject ) JToken.ReadFrom( jsonReader );
+							
+							foreach( KeyValuePair<string, JToken> kvp in jObj )
+							{
+								JArray jArray = ( JArray ) kvp.Value;
+
+								HashSet<KeyCode> hashSet = new HashSet<KeyCode>();
+								foreach( JToken jToken in jArray )
+								{
+									string keyCodeString = jToken.Value<string>();
+
+									if( !string.IsNullOrEmpty( keyCodeString ) )
+									{
+										KeyCode key;
+										if( System.Enum.TryParse( keyCodeString, out key ) && key != KeyCode.None )
+											hashSet.Add( key );
+									}
+								}
+
+								if( hashSet.Count > 0 )
+									binds.Add( kvp.Key, new List<KeyCode>( hashSet ).ToArray() );
+							}
+						}
+						catch { }
+					}
+				}
+
+				hasLoaded = true;
+				loading = false;
+
+				return binds;
+			} ),
+			( Dictionary<string,KeyCode[]> binds ) =>
+			{
+				foreach( KeyValuePair<string, KeyCode[]> kvp in binds )
+				{
+					SetKeyCodes( kvp.Key, kvp.Value );
+				}
+			} );
+		}
+	}
+}
---
+++ Runtime/Globals/KeyEventSingleton.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Tehelee.Baseline
+{
+	[System.Serializable]
+	public class KeyEvent
+	{
+		public string preferencesKey = string.Empty;
+
+		public KeyCode[] keys;
+
+		public bool invokeDuringInput = false;
+
+		public UnityEvent onDown = new UnityEvent();
+		public UnityEvent onUp = new UnityEvent();
+
+		public delegate void OnKey( KeyCode keyCode );
+		public event OnKey onKeyDown;
+		public event OnKey onKeyUp;
+
+		public void InvokeDown( KeyCode keyCode )
+		{
+			onDown?.Invoke();
+			onKeyDown?.Invoke( keyCode );
+		}
+
+		public void InvokeUp( KeyCode keyCode )
+		{
+			onUp?.Invoke();
+			onKeyUp?.Invoke( keyCode );
+		}
+	}
+	
+	public interface IKeyEventReceiver
+	{
+		IList<KeyEvent> GetKeyEvents();
+
+		bool OnKeyDown( KeyCode keyCode );
+		bool OnKeyUp( KeyCode keyCode );
+	}
+	
+    public static class KeyEventSingleton
+	{
+		private static HashSet<IKeyEventReceiver> listeners = new HashSet<IKeyEventReceiver>();
+		public static int listenersCount => listeners.Count;
+
+		private static Dictionary<KeyCode, HashSet<IKeyEventReceiver>> lookup = new Dictionary<KeyCode, HashSet<IKeyEventReceiver>>();
+
+		private static Dictionary<IKeyEventReceiver, float> registerTime = new Dictionary<IKeyEventReceiver, float>();
+
+		private static KeyCode[] allKeys = new KeyCode[ 0 ];
+		private static bool[] keyState = new bool[ 0 ];
+		
+		public delegate void OnEditingInputChanged( bool editingInput );
+		public static event OnEditingInputChanged onEditingInputChanged;
+
+		private static bool _editingInput = false;
+		public static bool editingInput
+		{
+			get => _editingInput;
+			private set
+			{
+				if( _editingInput != value )
+				{
+					_editingInput = value;
+					onEditingInputChanged?.Invoke( _editingInput );
+				}
+			}
+		}
+		
+		private static bool resetEditingInput = false;
+
+		public static void Register( IKeyEventReceiver keyEvents )
+		{
+			if( keyEvents == null )
+				return;
+
+			listeners.Add( keyEvents );
+
+			registerTime.Add( keyEvents, Time.time );
+
+			if( object.Equals( null, _IKeyWatcher ) )
+				_IKeyWatcher = Utils.StartCoroutine( IKeyWatcher() );
+
+			foreach( KeyEvent keyEvent in keyEvents.GetKeyEvents() )
+			{
+				foreach( KeyCode keyCode in keyEvent.keys )
+				{
+					if( !lookup.ContainsKey( keyCode ) )
+						lookup.Add( keyCode, new HashSet<IKeyEventReceiver>() );
+
+					lookup[ keyCode ].Add( keyEvents );
+				}
+			}
+
+			Dictionary<KeyCode, bool> state = new Dictionary<KeyCode, bool>();
+			for( int i = 0, iC = allKeys.Length; i < iC; i++ )
+				state.Add( allKeys[ i ], keyState[ i ] );
+
+			allKeys = new List<KeyCode>( lookup.Keys ).ToArray();
+			keyState = new bool[ allKeys.Length ];
+
+			for( int i = 0, iC = allKeys.Length; i < iC; i++ )
+				if( state.ContainsKey( allKeys[ i ] ) )
+					keyState[ i ] = state[ allKeys[ i ] ];
+		}
+
+		public static void UnRegister( IKeyEventReceiver keyEvents )
+		{
+			if( object.Equals( null, keyEvents ) || !listeners.Contains( keyEvents ) )
+				return;
+
+			listeners.Remove( keyEvents );
+
+			if( registerTime.ContainsKey( keyEvents ) )
+				registerTime.Remove( keyEvents );
+
+			if( listeners.Count == 0 )
+			{
+				Utils.StopCoroutine( _IKeyWatcher );
+				_IKeyWatcher = null;
+
+				lookup.Clear();
+
+				allKeys = new KeyCode[ 0 ];
+				keyState = new bool[ 0 ];
+			}
+			else
+			{
+				List<KeyCode> cleanup = new List<KeyCode>();
+				Dictionary<KeyCode, HashSet<IKeyEventReceiver>> modified = new Dictionary<KeyCode, HashSet<IKeyEventReceiver>>();
+
+				foreach( KeyCode keyCode in lookup.Keys )
+				{
+					HashSet<IKeyEventReceiver> hashSet = lookup[ keyCode ];
+					if( hashSet.Contains( keyEvents ) )
+						hashSet.Remove( keyEvents );
+
+					if( hashSet.Count == 0 )
+						cleanup.Add( keyCode );
+					else
+						modified.Add( keyCode, hashSet );
+				}
+
+				foreach( KeyCode keyCode in modified.Keys )
+					if( lookup.ContainsKey( keyCode ) )
+						lookup[ keyCode ] = modified[ keyCode ];
+
+				foreach( KeyCode keyCode in cleanup )
+					lookup.Remove( keyCode );
+
+				Dictionary<KeyCode, bool> state = new Dictionary<KeyCode, bool>();
+				for( int i = 0, iC = allKeys.Length; i < iC; i++ )
+					state.Add( allKeys[ i ], keyState[ i ] );
+
+				allKeys = new List<KeyCode>( lookup.Keys ).ToArray();
+				keyState = new bool[ allKeys.Length ];
+
+				for( int i = 0, iC = allKeys.Length; i < iC; i++ )
+					if( state.ContainsKey( allKeys[ i ] ) )
+						keyState[ i ] = state[ allKeys[ i ] ];
+			}
+		}
+
+		private static Dictionary<KeyCode, List<IKeyEventReceiver>> triggerKeyDownEvents = new Dictionary<KeyCode, List<IKeyEventReceiver>>();
+		private static Dictionary<KeyCode, List<IKeyEventReceiver>> triggerKeyUpEvents = new Dictionary<KeyCode, List<IKeyEventReceiver>>();
+		
+		private static Coroutine _IKeyWatcher = null;
+		private static IEnumerator IKeyWatcher()
+		{
+			while( !KeyBindings.hasLoaded )
+				yield return null;
+
+			while( true )
+			{
+				triggerKeyDownEvents.Clear();
+				triggerKeyUpEvents.Clear();
+
+				bool _editingInput = false;
+				UnityEngine.UI.Selectable selectable = null;
+				if( Utils.IsObjectAlive( EventSystem.current ) && Utils.IsObjectAlive( EventSystem.current.currentSelectedGameObject ) )
+					selectable = EventSystem.current.currentSelectedGameObject.GetComponent<UnityEngine.UI.Selectable>();
+				
+				if( Utils.IsObjectAlive( selectable ) )
+				{
+					System.Type selectableType = selectable.GetType();
+					
+					if( ( typeof( TMPro.TMP_InputField ).EqualsOrAssignable( selectableType ) ) )
+					{
+						TMPro.TMP_InputField inputField = ( TMPro.TMP_InputField ) selectable;
+						_editingInput = inputField.gameObject.activeSelf && inputField.enabled && inputField.isFocused;
+					}
+					else if( typeof( UnityEngine.UI.InputField ).EqualsOrAssignable( selectableType ) )
+					{
+						UnityEngine.UI.InputField inputField = ( UnityEngine.UI.InputField ) selectable;
+						_editingInput = inputField.gameObject.activeSelf && inputField.enabled && inputField.isFocused;
+					}
+				}
+
+				if( resetEditingInput )
+				{
+					if( _editingInput )
+						editingInput = true;
+					else
+						editingInput = false;
+
+					resetEditingInput = false;
+				}
+				else if( editingInput != _editingInput )
+				{
+					if( _editingInput )
+						editingInput = true;
+					else
+						resetEditingInput = true;
+				}
+
+				bool pressed = false;
+				for( int i = 0, iC = allKeys.Length; i < iC; i++ )
+				{
+					KeyCode keyCode = allKeys[ i ];
+					pressed = Input.GetKey( keyCode );
+
+					if( pressed != keyState[ i ] )
+					{
+						HashSet<IKeyEventReceiver> _triggers = new HashSet<IKeyEventReceiver>();
+						foreach( IKeyEventReceiver keyEvents in lookup[ keyCode ] )
+							_triggers.Add( keyEvents );
+
+						List<IKeyEventReceiver> triggers = new List<IKeyEventReceiver>( _triggers );
+						triggers.Sort
+						(
+							( IKeyEventReceiver a, IKeyEventReceiver b ) =>
+							{
+								if( object.Equals( null, a ) || object.Equals( null, b ) )
+									return 0;
+								if( !registerTime.ContainsKey( a ) || !registerTime.ContainsKey( b ) )
+									return 0;
+
+								return -registerTime[ a ].CompareTo( registerTime[ b ] );
+							}
+						);
+
+						if( pressed )
+							triggerKeyDownEvents.Add( keyCode, triggers );
+						else
+							triggerKeyUpEvents.Add( keyCode, triggers );
+
+						keyState[ i ] = pressed;
+					}
+				}
+
+				foreach( KeyCode keyCode in triggerKeyDownEvents.Keys )
+					foreach( IKeyEventReceiver keyEvents in triggerKeyDownEvents[ keyCode ] )
+						if( keyEvents.OnKeyDown( keyCode ) )
+							break;
+
+				foreach( KeyCode keyCode in triggerKeyUpEvents.Keys )
+					foreach( IKeyEventReceiver keyEvents in triggerKeyUpEvents[ keyCode ] )
+						if( keyEvents.OnKeyUp( keyCode ) )
+							break;
+
+				yield return null;
+			}
+		}
+	}
+}
