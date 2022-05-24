ef905c303b331fe20ef1c1010c7cefe6c65a2d0c
Tehelee <Shaander1@gmail.com>
2022-05-24 08:41:05 -0700

    Update Utils.cs

--- Runtime/Utils.cs
+++ Runtime/Utils.cs
@@ -1551,9 +1551,6 @@ namespace Tehelee.Baseline
 				rigidbody.position += ( targetParent.transform.TransformPoint( moveWithSnapshot.localPosition ) - moveWithSnapshot.oldPosition );
 				Quaternion rotationDelta = targetParent.rotation * Quaternion.Inverse( moveWithSnapshot.oldRotation );
 				rigidbody.rotation *= rotationDelta;
-
-				rigidbody.position = rigidbody.position;
-				rigidbody.rotation = rigidbody.rotation;
 				
 				rigidbody.velocity = velocity;
 				rigidbody.angularVelocity = angularVelocity;
